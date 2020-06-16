b5175253c24c8182c18add2e0a2096a34a2ac677
Jeremy Woo <Jeremy.Woo@c3posttrade.com>
2020-06-17 08:44:35 +1200

    Init Repository

---
+++ TimedService.cs
@@ -0,0 +1,49 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks; 
+
+public class TimedBasedService : IHostedService, IDisposable
+{
+    private int executionCount = 0;
+    private readonly ILogger<TimedBasedService> _logger;
+    private Timer _timer;
+
+    public TimedBasedService(ILogger<TimedBasedService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Timed Hosted Service running.");
+
+        _timer = new Timer(DoWork, null, TimeSpan.Zero, 
+            TimeSpan.FromSeconds(5));
+
+        return Task.CompletedTask;
+    }
+
+    private void DoWork(object state)
+    {
+        var count = Interlocked.Increment(ref executionCount);
+
+        _logger.LogInformation(
+            "Timed Hosted Service is working. Count: {Count}", count);
+    }
+
+    public Task StopAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Timed Hosted Service is stopping.");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace processWorker
+{
+    public class BackgroundWorkerService : BackgroundService
+    {
+        private readonly ILogger<BackgroundWorkerService> _logger;
+
+        public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
