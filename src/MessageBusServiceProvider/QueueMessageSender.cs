f5e7be41023e740c8cade609a06fbe94e8a37f12
Jeremy Woo <Jeremy.Woo@c3posttrade.com>
2020-06-17 16:29:11 +1200

    Added more logging info.

--- src/MessageBusServiceProvider/QueueMessageSender.cs
+++ src/MessageBusServiceProvider/QueueMessageSender.cs
@@ -2,20 +2,24 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Logging;
 
 namespace MessageBusServiceProvider
 {
     public class QueueMessageSender
     {
         private readonly IQueueClient _queueClient;
+        private readonly ILogger _logger;
 
-        public QueueMessageSender(IQueueClient queueClient)
+        public QueueMessageSender(IQueueClient queueClient, ILogger logger)
         {
             this._queueClient = queueClient;
+            _logger = logger;
         }
 
         public async Task SendMessagesAsync(string messageSource)
         {
+            this._logger.LogInformation($"Sending message : {messageSource}");
             try
             {
                 var message = new Message(Encoding.UTF8.GetBytes(messageSource));
@@ -23,7 +27,7 @@ namespace MessageBusServiceProvider
             }
             catch (Exception exception)
             {
-
+                this._logger.LogInformation($"{exception.Message}");
             }
         }
     }
