6697ebe2fdf3f5466f6a6efd542d924feca8996d
jeremywwk <devaksaz@outlook.com>
2020-06-21 13:48:08 +0800

    AttendanceRecord data update

--- src/ThermoDataModel/Models/AttendanceRecord.cs
+++ src/ThermoDataModel/Models/AttendanceRecord.cs
@@ -22,16 +22,42 @@ namespace Service.ThermoDataModel.Models
     public class AttendanceRecord
     {
         public string Address { get; set; }
+        
         public int Age { get; set; }
+        
+        public string Birth { get; set; }
 
-        //public int AttendRecordId { get; set; }
-        //public int Id { get; set; }
-        //public string BodyTemperature { get; set; }
-        //public string Deviceid { get; set; }
-        //public string PersonId { get; set; }
-        //public int? Respirator { get; set; }
-        //public DateTime? Timestamp { get; set; }
-        //public string Userid { get; set; }
-        //public string ImgBase64 { get; set; }
+        public string BodyTemperature { get; set; }
+
+        public string CertificateNumber { get; set; }
+
+        public int CertificateType { get; set; }
+
+        public string Country { get; set; }
+
+        public int DeviceId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Gender { get; set; }
+
+        public string GroupId { get; set; }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Nation { get; set; }
+
+        public string PersonId { get; set; }
+
+        public string Phone { get; set; }
+
+        public int Respirator { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public string UserId { get; set; }
+
     }
 }
