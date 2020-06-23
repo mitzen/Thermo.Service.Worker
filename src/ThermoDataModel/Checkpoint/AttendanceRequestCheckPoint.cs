8d7e849b8dd0ccf0b76d5e1cbcba7794e9c8ddff
Jeremy Woo <Jeremy.Woo@c3posttrade.com>
2020-06-23 15:48:59 +1200

    Added checkpointing

---
+++ src/ThermoDataModel/Checkpoint/AttendanceRequestCheckPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Service.ThermoDataModel.Checkpoint
+{
+    public class AttendanceRequestCheckPoint
+    {
+        public double SequenceId { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        public double   LastSequence { get; set; }
+    }
+}
