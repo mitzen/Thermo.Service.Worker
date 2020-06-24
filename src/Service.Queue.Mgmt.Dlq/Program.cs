ecfd65760b63c95bd36550d3ef6c58112fdc445f
Jeremy Woo <Jeremy.Woo@c3posttrade.com>
2020-06-24 14:01:22 +1200

    init code changes

---
+++ src/Service.Queue.Mgmt.Dlq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Service.Queue.Mgmt.Dlq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
