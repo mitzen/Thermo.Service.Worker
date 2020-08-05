61499c51fa6a7a67e2346c2b662691bc249b41bd
jeremywwk <devaksaz@outlook.com>
2020-08-05 16:46:43 +0800

    Added interface for IPersonDataService

---
+++ src/AzCloudApp.MessageProcessor.Core.Thermo.DataServiceProvider/IPersonDataService.cs
@@ -0,0 +1,9 @@
+﻿using AzCloudApp.MessageProcessor.Core.Thermo.DataStore.DataStoreModel;
+
+namespace AzCloudApp.MessageProcessor.Core.Thermo.DataServiceProvider
+{
+    public interface IPersonDataService
+    {
+        UsersDataStore GetUserByName(string username);
+    }
+}
